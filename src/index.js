29318ef164a629b246288166e9892fe895db7aa5
Sergey_Opihaylenko <sergomail@bk.ru>
2022-10-23 20:13:00 +0300

    edit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const App = (params) => {
+  return <h1>APP</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
