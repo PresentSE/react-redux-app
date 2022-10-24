31794d76ced764e6e9f98635863bbff393aa453b
Sergey_Opihaylenko <sergomail@bk.ru>
2022-10-25 00:05:26 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
+function createStore(initialState) {
+  let state = initialState;
+
+  function getState() {
+    return state;
+  }
+  function dispatch(action) {
+    console.log(action);
+    if (action.type === "task/completed") {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      state = newArray;
+      console.log(state);
+    }
+  }
+  return { getState, dispatch };
+}
+const store = createStore([{ id: 1, description: "Task 1", completed: false }]);
+
 const App = (params) => {
-  return <h1>APP</h1>;
+  console.log(store.getState());
+
+  return (
+    <>
+      <h1>APP</h1>
+      <button
+        onClick={() =>
+          store.dispatch(
+            store.dispatch({ type: "task/completed", payload: { id: 1 } })
+          )
+        }
+      >
+        Complete
+      </button>
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
